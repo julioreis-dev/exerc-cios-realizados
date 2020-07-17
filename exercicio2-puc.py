beb46246e860248cc9b1243891a3eb48c9f2627a
Julio dos Reis Firmino <67196397+julioreis-dev@users.noreply.github.com>
2020-07-16 23:20:22 -0300

    Add files via upload

---
+++ exercicio2-puc.py
@@ -0,0 +1,10 @@
+def triangular(numero):
+    return numero+1
+
+numero_informado = int(input('digite um numero?'))
+x=triangular(numero_informado)
+
+for t in range (1, x):
+    for n in range(1, t+1):
+        print(n, end='')
+    print('')
